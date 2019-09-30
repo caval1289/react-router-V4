452651f8c26bcc9df1303fe2cfb915e137eccb91
Alessandro Cavaleri <caval1289@gmail.com>
2019-09-30 20:56:41 +0200

    mise un place HOC

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,15 @@ import Header from '../containers/header';
 import { Route, Switch } from "react-router-dom";
 import Home from "./home";
 import Ressources from "./ressources";
+import RequireAuthentification from "../helpers/require-authentification";
 
 function App() {
   return (
     <div>
       <Header />
       <Switch>
-        <Route exact path="/:id" component={Home} />
-        <Route exact path="/ressources" component={Ressources} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/ressources" component={RequireAuthentification(Ressources)} />
       </Switch>
     </div>
   );
