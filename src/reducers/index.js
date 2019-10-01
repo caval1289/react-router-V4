7a4457ec36a995839866ea68b82dd4d4a3382332
Alessandro Cavaleri <caval1289@gmail.com>
2019-10-01 17:30:32 +0200

    1 ere parti middleware

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import AuthentificationReducer from "./authentification";
+import ActionInfoReducer from "./action-info";
 
 const rootReducer = combineReducers({
-    authentification: AuthentificationReducer
+    authentification: AuthentificationReducer,
+    actionInfo: ActionInfoReducer
 });
 
 export default rootReducer;
