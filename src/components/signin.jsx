efbd8462ef2dbb3acbbec3f52af93aa551f4f97b
cavaleri <caval1289@gmail.com>
2020-02-21 16:09:48 +0100

    signin

---
+++ src/components/signin.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Field, reduxForm} from "redux";
+import  * as actions from "../actions";
+import { connect} from "react-redux";
+
+class signin extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default signin;
