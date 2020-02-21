2974381eddd2869feff9684085bab43a43c720b2
cavaleri <caval1289@gmail.com>
2020-02-21 17:32:27 +0100

    formulaire + redux form

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Home from "./home";
 import TodoApp from "./todo-app"
 import Ressources from "./ressources";
 import RequireAuthentification from "../helpers/require-authentification";
+import Signin from "./signin";
 
 function App() {
   return (
@@ -12,8 +13,10 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={Home} />
+        
         <Route exact path="/liste" component={RequireAuthentification(TodoApp)} />
         <Route exact path="/ressources" component={RequireAuthentification(Ressources)} />
+        <Route exact path ="/signin" component={Signin}/>
       </Switch>
     </div>
   );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 import AuthentificationReducer from "./authentification";
 import ActionInfoReducer from "./action-info";
 import RessourceReducer from "./ressources";
+import { reducer as form } from "redux-form";
 
 const rootReducer = combineReducers({
+    form,
     authentification: AuthentificationReducer,
     actionInfo: ActionInfoReducer,
     ressource: RessourceReducer
